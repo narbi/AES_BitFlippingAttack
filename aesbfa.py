18dc8812e2ec6265f1f155ebc18894bc3e4cb3c2
Christina <skouloudi@gmail.com>
2015-09-29 14:41:27 +0300

    Update and rename aesbitflipattack.py to aesbfa.py

--- aesbfa.py
+++ aesbfa.py
@@ -1,6 +1,6 @@
 #   AES Bit Flipping Attack 
-#   Description: Purpose of this script is the proof of concept for the revalidation of an expired ticket 
-#   in order to get authenticated. In this scenario, the ticket that gives the authorization for a change 
+#   Description: Purpose of this script is the proof of concept for the revalidation of a forged expired ticket 
+#   in order to get authenticated and perfornm an action as the original user. In this scenario, the ticket that gives the authorization for a change 
 #   or a confirmation to a request is a url which has three fields: username, email and validity period. 
 #   We perform a bit flip attack to change the validity period field of the url parameter.
 #   This demo is done for educational purposes only.
